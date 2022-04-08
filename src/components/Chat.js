640a014b98d5394efc1da91c8d891cd185838f2f
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-08 14:31:33 +0430

    add chat engine

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "firebase";
+import { useNavigate } from "react-router-dom";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const navigate = useNavigate();
+
+  const handelLogout = async () => {
+    await auth().signOut();
+    navigate("/");
+  };
+
+  return (
+    <div className="chats-page">
+      <nav className="nav-bar">
+        <div className="logo-tab">UniChat</div>
+        <div onClick={handelLogout} className="logout-tab">
+          Logout
+        </div>
+      </nav>
+
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectId="a7cee904-a677-492a-a627-2a240a5d6a12"
+        userName=""
+        userSecret=""
+      />
+    </div>
+  );
 };
 
 export default Chat;
