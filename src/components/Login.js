e0655e946109b0f6f5c628840181e21d727e1f68
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-08 12:28:34 +0430

    add context file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,21 @@
 import React from "react";
+import "firebase/app";
 import { GoogleOutlined } from "@ant-design/icons";
 
+import { auth } from "../firebase/server";
+import firebase from "firebase/app";
+
 const Login = () => {
+  const loginHandler = () => {
+    auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  };
+
   return (
     <div id="login-page">
       <div id="login-card">
         <h2>Welcome to UniChat</h2>
 
-        <div className="login-button google">
+        <div className="login-button google" onClick={loginHandler}>
           <GoogleOutlined /> Sign In With Google
         </div>
       </div>
